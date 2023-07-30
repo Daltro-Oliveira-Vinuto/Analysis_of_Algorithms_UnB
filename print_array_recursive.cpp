2feef7154d840e1001c3ad997011bed5fa2434b4
Daltro-Oliveira-Vinuto <daltroov777@gmail.com>
2023-07-30 08:02:13 -0300

    print array recursive analised

--- print_array_recursive.cpp
+++ print_array_recursive.cpp
@@ -1,5 +1,7 @@
 typedef long long ll;
 
+// In the worst case is O(n) by master method
+// using the substituion n = log m for T(n)=T(n-1)+a, T(0)=b
 void print_recursive(ll* array, ll size) {
 	if (size > 0) {
 		print_recursive(array, size-1);
