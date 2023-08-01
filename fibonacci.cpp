d6b1c4d9195c0be7af2cb60fdbf8d4e8bd133dda
Daltro-Oliveira-Vinuto <daltroov777@gmail.com>
2023-08-01 16:49:07 -0300

    fibonacci corrected

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,8 +1,9 @@
 
 typedef long long ll;
 
-// The complexity is O(2^n) using master method
-// T(n) = T(n-1) + T(n-2) + 1 <= 2*T(n-1) + 1 = O(2^n)
+// The complexity is exponential using master method
+// T(n) = T(n-1) + T(n-2) + 1 <= 2*T(n-1) + 1 <= O(2^n)
+// T(n) = T(n-1) + T(n-2) + 1 >= 2*T(n-2) +1 >= O(sqrt(2)^n)
 ll fibonacci_slow(ll n) {
 	if (n == 0 || n == 1) return 1;
 	else {
